ae964de2810197c31656207e26e85d76802cdcba
MSP2202 <msp2202@yandex.ru>
2025-10-30 16:05:41 +0300

    hw01-gradle - Правки по замечаниям

--- hw01-gradle/build.gradle.kts
+++ hw01-gradle/build.gradle.kts
@@ -1,14 +1,41 @@
+/* Импорт класса для типобезопасной конфигурации задачи shadowJar */
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+/* Плагины для модуля */
 plugins {
-    id("java")
-    id("application")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("java")                              /* Плагин для Java компиляции */
+    id("application")                       /* Плагин для запуска приложений */
+    id("com.github.johnrengelman.shadow")   /* Плагин для создания толстый (fat jar) */
 }
 
+/* Конфигурация плагина application - указываем главный класс */
 application {
-    mainClass.set("ru.otus.HelloOtus")
+    mainClass.set("ru.otus.HelloOtus")  /* Главный класс приложения */
 }
 
-// Вместо стандартного jar используем shadowJar
-tasks.shadowJar {
-    archiveFileName = "hw01-gradle-fat.jar"
+/* Зависимости проекта */
+dependencies {
+    /* Библиотека Guava от Google - версия управляется в корневом build.gradle.kts */
+    implementation("com.google.guava:guava")
+}
+
+/* Конфигурация задач Gradle */
+tasks {
+    /* Конфигурация стандартной задачи jar для создания ТОНКОГО JAR */
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("hw01-gradle")  /* Базовое имя архива */
+        archiveVersion.set("1.0")           /* Версия архива */
+        archiveClassifier.set("")           /* Классификатор (пустой = основной артефакт) */
+
+        /* Конфигурация манифеста JAR файла */
+        manifest {
+            attributes(mapOf("Main-Class" to "ru.otus.HelloOtus"))  /* Главный класс для запуска */
+        }
+    }
+
+    /* Модификация стандартной задачи build */
+    build {
+        /* Задача build теперь зависит от shadowJar - при сборке создается fat jar */
+        dependsOn(shadowJar)
+    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,44 @@
+/* Настройка корневого проекта и включение модулей */
 rootProject.name = "2025_09_OTUS_JAVA_MSP"
 
-include("hw01-gradle")
+/* Включение подпроектов в сборку */
+include("hw01-gradle")  /* Модуль с первым домашним заданием */
+//include("L02-gradle2")
+//include("L02-gradle2-libApi")
+//include("L02-gradle2-libApiUse")
+//include("L02-logging")
+//include("L03-qa")
+//include("L04-generics")
+//include("L05-collections")
+//include("L06-annotations")
 
+/* Управление версиями плагинов через settings */
 pluginManagement {
+    /* Чтение версий плагинов из gradle.properties */
+    val jgitver: String by settings
+    val dependencyManagement: String by settings
+    val springframeworkBoot: String by settings
+    val johnrengelmanShadow: String by settings
+    val jib: String by settings
+    val protobufVer: String by settings
+    val sonarlint: String by settings
+    val spotless: String by settings
+
+    /* Настройка репозиториев для плагинов */
     repositories {
         gradlePluginPortal()
         mavenCentral()
     }
 
+    /* Регистрация плагинов с версиями */
     plugins {
-        id("io.spring.dependency-management") version "1.1.7"
-        id("org.springframework.boot") version "3.4.2"
+//        id("fr.brouillard.oss.gradle.jgitver") version jgitver
+        id("io.spring.dependency-management") version dependencyManagement
+        id("org.springframework.boot") version springframeworkBoot
+        id("com.github.johnrengelman.shadow") version johnrengelmanShadow
+//        id("com.google.cloud.tools.jib") version jib
+//        id("com.google.protobuf") version protobufVer
+//        id("name.remal.sonarlint") version sonarlint
+//        id("com.diffplug.spotless") version spotless
     }
 }
